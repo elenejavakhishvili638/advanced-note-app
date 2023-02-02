f251ce4c17700e10f8d5ced22e05a95c3e0effd8
elene638 <elenejavaxishvili@yahoo.com>
2023-02-02 10:05:12 -0800

    Handle note creation logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Container } from 'react-bootstrap'
+import { v4 as uuidv4 } from "uuid";
 import { Routes, Route, Navigate } from 'react-router'
 import NewNote from './pages/NewNote'
-import { Tag, RawNote } from './types/noteTypes'
+import { Tag, RawNote, NoteData } from './types/noteTypes'
 import { useLocalStorage } from './hooks/useLocalStorageHook'
 
 const App: React.FC = () => {
 
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return { ...note, tags: tags.filter((tag) => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  const createNote = ({ tags, ...data }: NoteData) => {
+    setNotes(prevNotes => {
+      return [...prevNotes, { ...data, id: uuidv4(), tagIds: tags.map((tag) => tag.id) }]
+    })
+  }
 
   return (
     <Container className='my-4'>
