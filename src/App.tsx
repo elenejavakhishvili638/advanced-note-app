15465e2e823d70887765d7d7f4fe42ce681cba84
elene638 <elenejavaxishvili@yahoo.com>
2023-02-02 00:09:58 -0800

    Handle basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Routes, Route, Navigate } from 'react-router'
+import NewNote from './pages/NewNote'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Container className='my-4'>
+      <Routes>
+        <Route path="/" element={"/"}  />
+        <Route path="/new-note" element={<NewNote/>} />
+        <Route path=":id">
+          <Route index element={"Note"} />
+          <Route path="edit-note" element={"Edit"} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   )
 }
 
---
+++ src/pages/NewNote.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./newNote.css"
+
+const NewNote = () => {
+  return (
+    <>
+      <h1 className='mb-4'>New Note</h1>
+    </>
+  )
+}
+
+export default NewNote
